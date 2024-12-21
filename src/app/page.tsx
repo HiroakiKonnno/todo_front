a30eef95640df8433c8e747d0cb13dc77efb75a9
hiro <sample@example.com>
2024-12-21 18:52:01 +0900

    ログイン機能作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,54 @@
 "use client";
 
+import { useState } from "react";
+import styles from "./styles/login.module.css";
+import apiClient from "@/lib/axios";
+import { useRouter } from "next/navigation";
+
 export default function Welcome() {
+  const [loginId, setLoginId] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      await apiClient.post(`/signin`, {
+        login_id: loginId,
+        password: password,
+      });
+
+      alert("ログインしました！");
+      router.push("/tasks");
+    } catch (error) {
+      console.error("ログインできませんでした", error);
+      alert("ログインできませんでした");
+    }
+  };
+
   return (
-    <main>
-      <h1>タスク管理</h1>
-    </main>
+    <>
+      <h1 className={styles.title}>ログインページ</h1>
+      <form onSubmit={handleSubmit}>
+        <p className={styles.label}>ユーザID</p>
+        <input
+          value={loginId}
+          onChange={(e) => setLoginId(e.target.value)}
+          className={styles.inputField}
+        />
+        <p className={styles.label}>パスワード</p>
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className={styles.textareaField}
+        />
+        <div>
+          <button type="submit" className={styles.buttonPrimary}>
+            ログイン
+          </button>
+        </div>
+      </form>
+    </>
   );
 }
