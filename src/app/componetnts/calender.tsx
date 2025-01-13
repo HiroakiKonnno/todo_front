69e8104cc644f9f7433a27be322f97d86cba6f76
hiro <sample@example.com>
2025-01-13 19:47:26 +0900

    カレンダー機能追加

---
+++ src/app/componetnts/calender.tsx
@@ -0,0 +1,34 @@
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import styles from "../styles/calender.module.css";
+
+type Props = {
+  label: string;
+  id: string;
+  handleDateChange: (date: Date) => void;
+  selectedDate: Date;
+};
+
+export const Calender = ({
+  label,
+  id,
+  handleDateChange,
+  selectedDate,
+}: Props) => {
+  return (
+    <div>
+      <label htmlFor="datetime" className={styles.label}>
+        {label}
+      </label>
+      <DatePicker
+        id={id}
+        selected={selectedDate}
+        onChange={(date) => handleDateChange(date as Date)}
+        showTimeSelect={false}
+        dateFormat="yyyy/MM/dd"
+        placeholderText="日時を選択"
+        className={styles.form}
+      />
+    </div>
+  );
+};
--- src/app/tasks/[id]/page.tsx
+++ src/app/tasks/[id]/page.tsx
@@ -8,6 +8,8 @@ import { useEffect, useState } from "react";
 import styles from "../../styles/taskForm.module.css";
 import { Header } from "@/app/componetnts/header";
 import { useFlashMessage } from "@/app/context/FlashMessageContext";
+import { Calender } from "@/app/componetnts/calender";
+import { format } from "date-fns";
 
 export default function TaskDetailPage() {
   const params = useParams();
@@ -23,6 +25,24 @@ export default function TaskDetailPage() {
     },
     staleTime: 0,
   });
+  const [selectedStartDate, setSelectedStartDate] = useState<string>(
+    data?.start_date
+      ? format(data.start_date, "yyyy-MM-dd")
+      : format(new Date(), "yyyy-MM-dd")
+  );
+  const [selectedEndDate, setSelectedEndDate] = useState<string>(
+    data?.end_date
+      ? format(data.end_date, "yyyy-MM-dd")
+      : format(new Date(), "yyyy-MM-dd")
+  );
+
+  const handleStartDateChange = (date: Date) => {
+    setSelectedStartDate(format(date, "yyyy-MM-dd"));
+  };
+
+  const handleEndDateChange = (date: Date) => {
+    setSelectedEndDate(format(date, "yyyy-MM-dd"));
+  };
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -32,6 +52,8 @@ export default function TaskDetailPage() {
       await apiClient.patch(`/tasks/${id}`, {
         title: title,
         content: content,
+        start_date: selectedStartDate,
+        end_date: selectedEndDate,
       });
 
       setFlashMessage("更新に成功しました！");
@@ -58,6 +80,16 @@ export default function TaskDetailPage() {
     if (data) {
       setTitle(data.title);
       setContent(data.content);
+      setSelectedStartDate(
+        data.start
+          ? format(data.start_date, "yyyy-MM-dd")
+          : format(new Date(), "yyyy-MM-dd")
+      );
+      setSelectedEndDate(
+        data.end
+          ? format(data.end_date, "yyyy-MM-dd")
+          : format(new Date(), "yyyy-MM-dd")
+      );
     }
   }, [data]);
 
@@ -81,6 +113,24 @@ export default function TaskDetailPage() {
             value={content != undefined ? content : data.content}
             onChange={(e) => setContent(e.target.value)}
             className={styles.inputField}
+          />
+          <Calender
+            label="start"
+            id="start"
+            handleDateChange={handleStartDateChange}
+            selectedDate={
+              selectedStartDate != undefined
+                ? selectedStartDate
+                : data.start_date
+            }
+          />
+          <Calender
+            label="end"
+            id="end"
+            handleDateChange={handleEndDateChange}
+            selectedDate={
+              selectedEndDate != undefined ? selectedEndDate : data.end_date
+            }
           />
           <div>
             <button type="submit" className={styles.buttonPrimary}>
