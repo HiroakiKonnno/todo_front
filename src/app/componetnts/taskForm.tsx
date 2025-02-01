86d855ade6c2f05564688539a0e58a8fdd489b59
hiro <sample@example.com>
2025-02-01 23:10:34 +0900

    task formを切り出し

--- src/app/componetnts/taskForm.tsx
+++ src/app/componetnts/taskForm.tsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../styles/taskForm.module.css";
 import { Calender } from "./calender";
 import { format, parse } from "date-fns";
 import apiClient from "@/lib/axios";
 import { useParams, useRouter } from "next/navigation";
 import { useFlashMessage } from "../context/FlashMessageContext";
+import { useSelector } from "react-redux";
+import { RootState } from "../store";
 
 type TaskFormProps = {
   titleData: string | null;
@@ -26,6 +28,20 @@ export const TaskForm: React.FC<TaskFormProps> = ({
     endDateData ? format(endDateData, "yyyy-MM-dd") : null
   );
 
+  const user = useSelector((state: RootState) => state.user.user);
+
+  useEffect(() => {
+    if (startDateData) {
+      setSelectedStartDate(format(startDateData, "yyyy-MM-dd"));
+    }
+  }, [startDateData]);
+
+  useEffect(() => {
+    if (endDateData) {
+      setSelectedEndDate(format(endDateData, "yyyy-MM-dd"));
+    }
+  }, [endDateData]);
+
   const handleStartDateChange = (date: Date) => {
     setSelectedStartDate(format(date, "yyyy-MM-dd"));
   };
@@ -34,14 +50,32 @@ export const TaskForm: React.FC<TaskFormProps> = ({
     setSelectedEndDate(format(date, "yyyy-MM-dd"));
   };
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-
+  const handleUpdate = async () => {
     try {
       // APIにPATCHリクエストを送信
       await apiClient.patch(`/tasks/${id}`, {
         title: title,
         content: content,
+        user_id: user?.id,
+        start_date: selectedStartDate,
+        end_date: selectedEndDate,
+      });
+
+      setFlashMessage("更新に成功しました！", "success");
+      router.push("/tasks");
+    } catch {
+      setFlashMessage("更新に失敗しました！", "fail");
+      router.push("/tasks");
+    }
+  };
+
+  const handleCreate = async () => {
+    try {
+      // APIにPATCHリクエストを送信
+      await apiClient.post(`/tasks`, {
+        title: title,
+        content: content,
+        user_id: user?.id,
         start_date: selectedStartDate,
         end_date: selectedEndDate,
       });
@@ -54,14 +88,30 @@ export const TaskForm: React.FC<TaskFormProps> = ({
     }
   };
 
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (id) {
+      handleUpdate();
+    } else {
+      handleCreate();
+    }
+  };
+
+  const handleDelete = async () => {
+    const userConfirmed = window.confirm("本当に削除しますか？");
+    if (!userConfirmed) return;
+
+    await apiClient.delete(`/tasks/${id}`);
+    setFlashMessage("削除に成功しました！", "success");
+    router.push("/tasks");
+  };
+
   const [title, setTitle] = useState(titleData);
   const [content, setContent] = useState(contentData);
   const params = useParams();
   const { id } = params;
   const router = useRouter();
   const { setFlashMessage } = useFlashMessage();
-
-  console.log(selectedEndDate);
 
   return (
     <div className={styles.container}>
@@ -105,6 +155,11 @@ export const TaskForm: React.FC<TaskFormProps> = ({
           <button type="submit" className={styles.buttonPrimary}>
             {id ? "更新" : "作成"}
           </button>
+          {id && (
+            <button className={styles.buttonDanger} onClick={handleDelete}>
+              削除
+            </button>
+          )}
         </div>
       </form>
     </div>
